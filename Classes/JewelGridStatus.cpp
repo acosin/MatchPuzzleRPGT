c642fd076fdcdef0e7fd802f97f4223bcab215e5
suiyan-li <suiyan.li@dena.com>
2015-10-20 14:53:06 +0900

    add JewelInfo, MatchedJewels, MatchCombo, JewelGridStatus

---
+++ Classes/JewelGridStatus.cpp
@@ -0,0 +1,158 @@
+//
+//  JewelGridStatus.cpp
+//  MatchPuzzleRPGT
+//
+//
+
+#include "JewelGridStatus.h"
+
+//NOTE: not check >0 here, check startCol <= endCol
+MatchedJewels* MatchedJewels::createMatchRow(int row, int startCol, int endCol, ElementType type)
+{
+    if (startCol > endCol)
+        return nullptr;
+    
+    auto ret = new MatchedJewels();
+    ret->addMatchRowIgnoreDuplication(row, startCol, endCol, type);
+    return ret;
+}
+
+MatchedJewels* MatchedJewels::createMatchCol(int col, int startRow, int endRow, ElementType type)
+{
+    if (startRow > endRow)
+        return nullptr;
+    
+    auto ret = new MatchedJewels();
+    ret->addMatchColIgnoreDuplication(col, startRow, endRow, type);
+    return ret;
+}
+
+void MatchedJewels::addMatchRowIgnoreDuplication(int row, int startCol, int endCol, ElementType type)
+{
+    if (startCol > endCol) {
+        return;
+    }
+    
+    for (int i = startCol; i <= endCol; i++) {
+        jewelset.insert(JewelInfo(i, row, type));
+    }
+    rows.insert(row);
+}
+
+void MatchedJewels::addMatchColIgnoreDuplication(int col, int startRow, int endRow, ElementType type)
+{
+    if (startRow > endRow)
+        return;
+    
+    for (int i = startRow; i <= endRow; i++) {
+        jewelset.insert(JewelInfo(col, i, type));
+    }
+    cols.insert(col);
+}
+
+int MatchedJewels::getRowCount()
+{
+    return rows.size();
+}
+
+int MatchedJewels::getColCount()
+{
+    return cols.size();
+}
+
+ElementType MatchedJewels::getType()
+{
+    return (*jewelset.begin()).type;
+}
+
+// -- MatchCombo --
+
+int MatchCombo::getRowCount()
+{
+    int ret = 0;
+    for (auto match : matches) {
+        ret += match->getRowCount();
+    }
+    return ret;
+}
+
+int MatchCombo::getColCount()
+{
+    int ret = 0;
+    for (auto match : matches) {
+        ret += match->getColCount();
+    }
+    return ret;
+}
+
+int MatchCombo::getRowOfTypeCount(ElementType type)
+{
+    int ret = 0;
+    for (auto match : matches) {
+        if (match->getType() == type) {
+            ret += match->getRowCount();
+        }
+    }
+    return ret;
+}
+
+int MatchCombo::getColOfTypeCount(ElementType type)
+{
+    int ret = 0;
+    for (auto match : matches) {
+        if (match->getType() == type) {
+            ret += match->getColCount();
+        }
+    }
+    return ret;
+}
+
+// -- JewelGridStatus --
+
+JewelGridStatus::JewelGridStatus():
+isCrushing(false),
+comboCount(0)
+{
+    
+}
+
+JewelGridStatus::~JewelGridStatus()
+{
+    
+}
+
+int JewelGridStatus::getRowCount()
+{
+    int ret = 0;
+    for (auto combo : comboes) {
+        ret += combo->getRowCount();
+    }
+    return ret;
+}
+
+int JewelGridStatus::getColCount()
+{
+    int ret = 0;
+    for (auto combo : comboes) {
+        ret += combo->getColCount();
+    }
+    return ret;
+}
+
+int JewelGridStatus::getRowOfTypeCount(ElementType type)
+{
+    int ret = 0;
+    for (auto combo : comboes) {
+        ret += combo->getRowOfTypeCount(type);
+    }
+    return ret;
+}
+
+int JewelGridStatus::getColOfTypeCount(ElementType type)
+{
+    int ret = 0;
+    for (auto combo : comboes) {
+        ret += combo->getColOfTypeCount(type);
+    }
+    return ret;
+}
---
+++ Classes/JewelGridStatus.h
@@ -0,0 +1,96 @@
+//
+//  JewelGridStatus.h
+//  MatchPuzzleRPGT
+//
+//
+
+#pragma once
+
+#include <set>
+#include <vector>
+
+#include "ElementType.h"
+#include "Jewel.h"
+
+class JewelInfo
+{
+public:
+    int x,y;
+    ElementType type;
+public:
+    JewelInfo(int x,int y, ElementType type) {
+        set(x, y, type);
+    }
+    
+    void set(int x, int y, ElementType type) {
+        this->x = x;
+        this->y = y;
+        this->type = type;
+    }
+    
+    // only check pos
+    bool operator==(const JewelInfo& rhs) const
+    {
+        return (x == rhs.x && y == rhs.y);
+    }
+    
+    bool operator<(const JewelInfo& rhs) const
+    {
+        return (x < rhs.x && y < rhs.y);
+    }
+    
+    static JewelInfo* createJewelInfo(Jewel* jewel)
+    {
+        return new JewelInfo(jewel->getX(),
+                             jewel->getY(),
+                             jewel->getType());
+    }
+};
+
+
+//TODO: may use other storing style
+class MatchedJewels
+{
+public:
+    //NOTE: store by the order [x][y]
+    std::set<JewelInfo> jewelset;
+    std::set<int> rows;
+    std::set<int> cols;
+public:
+    static MatchedJewels* createMatchRow(int row, int startCol, int endCol, ElementType type);
+    static MatchedJewels* createMatchCol(int col, int startRow, int endRow, ElementType type);
+    
+    void addMatchRowIgnoreDuplication(int row, int startCol, int endCol, ElementType type);
+    void addMatchColIgnoreDuplication(int col, int startRow, int endRow, ElementType type);
+    
+    int getRowCount();
+    int getColCount();
+    ElementType getType();
+};
+
+class MatchCombo
+{
+public:
+    std::vector<MatchedJewels*> matches;
+public:
+    int getRowCount();
+    int getColCount();
+    int getRowOfTypeCount(ElementType type);
+    int getColOfTypeCount(ElementType type);
+};
+
+class JewelGridStatus
+{
+public:
+    bool isCrushing;
+    int comboCount;
+    std::vector<MatchCombo*> comboes;
+public:
+    JewelGridStatus();
+    ~JewelGridStatus();
+    
+    int getRowCount();
+    int getColCount();
+    int getRowOfTypeCount(ElementType type);
+    int getColOfTypeCount(ElementType type);
+};
