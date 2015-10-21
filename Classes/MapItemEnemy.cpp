c2d01f12b2f03e7727e7009c38e65d22b604d5de
suiyan-li <suiyan.li@dena.com>
2015-10-21 15:56:24 +0900

    add MaptItemEnemy

--- Classes/MapItemEnemy.cpp
+++ Classes/MapItemEnemy.cpp
@@ -5,3 +5,24 @@
 //
 
 #include "MapItemEnemy.h"
+
+
+MapItemEnemy::MapItemEnemy(int x, int y)
+{
+    MapItemEnemy::MapItemEnemy(x,y,false);
+}
+
+MapItemEnemy::MapItemEnemy(int x, int y, bool movable)
+{
+    MapItemBase::MapItemBase(x,y,movable,MapItemType::Enemy);
+}
+
+MapItemEnemy::MapItemEnemy()
+{
+    
+}
+
+void MapItemEnemy::setStatusData(EnemyStatusData *statusData)
+{
+    _statusData = statusData;
+}
--- Classes/MapItemEnemy.h
+++ Classes/MapItemEnemy.h
@@ -9,12 +9,18 @@
 #include "cocos2d.h"
 
 #include "MapItemBase.h"
+#include "EnemyStatusData.h"
 
 USING_NS_CC;
 
 class MapItemEnemy : public MapItemBase
 {
 private:
-    
+    EnemyStatusData* _statusData;
 public:
+    MapItemEnemy(int x, int y);
+    MapItemEnemy(int x, int y, bool movable);
+    MapItemEnemy();
+    
+    void setStatusData(EnemyStatusData* statusData);
 };
